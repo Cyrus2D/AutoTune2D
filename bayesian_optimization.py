2f1b97dfa3bfc724f71ea6ed7e4fda8fe3628354
aradfir <aradfir@gmail.com>
2022-06-03 12:59:59 +0430

    Changed space to only use choice, and use utils functions to populate choice

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,7 +1,7 @@
 import csv
 import signal
 from os.path import join
-
+import utils
 import hyperopt
 import hyperopt.pyll.stochastic
 import GenerateSettings
@@ -11,7 +11,8 @@ from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
 from ResultParser import get_result_data, short_result_to_dict
 
 space = {'ChainAction/ChainDeph': hp.choice('ChainAction/ChainDeph', [1, 2, 3, 4]),
-         'ChainAction/ChainNodeNumber': hp.quniform('ChainAction/ChainNodeNumber', 500, 1000, 100),
+         'ChainAction/ChainNodeNumber': hp.choice('ChainAction/ChainNodeNumber',
+                                                  utils.GenerateSpacedSamples(500, 1000, 100)),
          }
 i = 0
 space_sample = hyperopt.pyll.stochastic.sample(space)
